ff55ccb9b715e40cb411c1417c038e991c5246cc
Alesja Vassiljeva <alesja.vassiljeva@ivkhk.ee>
2021-09-19 23:05:14 +0300

    Summa_chisel

---
+++ src/summa_chisel/Summa_chisel.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package summa_chisel;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author vassi
+ */
+public class Summa_chisel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ввидите трёхзначное число: ");
+        int x = scanner.nextInt();
+        int a = x % 10;
+        int b = x / 10 % 10;
+        int c = x / 100;
+        int sum = a + b + c;
+        System.out.println("Единиц: " + a);
+        System.out.println("Десятков: " + b);
+        System.out.println("Сотен: " + c);
+        System.out.println("Сумма цифр: "+ sum);
+    }
+    
+}
+    
+    
